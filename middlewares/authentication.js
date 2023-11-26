ab2338bb39bd8761c205f5c9bc76ee18abc22358
willy836 <wilsonkimiri836@gmail.com>
2023-11-26 20:17:12 +0300

    Authentication middleware

---
+++ middlewares/authentication.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || authHeader.startsWith("Beare ")) {
+    throw new Error("Authentication invalid");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new Error("Authentication invalid");
+  }
+};
+
+module.exports = auth;
