7a4999e3bcaf7eee3c20e371479263bab3636da6
willy836 <wilsonkimiri836@gmail.com>
2023-11-26 21:24:40 +0300

    Add Reservation model

---
+++ models/Reservation.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const ReservationSchema = mongoose.Schema(
+  {
+    city: {
+      type: String,
+      required: [true, "Please provide city"],
+    },
+    startDate: {
+      type: Date,
+      required: [true, "Please provide start date"],
+    },
+    endDate: {
+      type: Date,
+      required: [true, "Please provide end date"],
+    },
+    authorId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide author"],
+    },
+    restaurantTableId: {
+      type: mongoose.Types.ObjectId,
+      ref: "RestaurantTable",
+      required: [true, "Please provide restaurant table"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Reservation", ReservationSchema);
