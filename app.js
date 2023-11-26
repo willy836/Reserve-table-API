519a3ea90fbc5537faa033ec775d79e50935e677
willy836 <wilsonkimiri836@gmail.com>
2023-11-26 15:13:07 +0300

    Add routes for register and login user

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 require("dotenv").config();
 
 const connectDB = require("./db/connect");
+const authRouter = require("./routes/auth");
 
 const app = express();
+
+app.use(express.json());
+
+// routes
+app.use("/api/v1/auth", authRouter);
 
 const port = process.env.PORT || 3000;
 
