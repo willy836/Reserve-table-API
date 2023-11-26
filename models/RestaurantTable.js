bc2f15a423925cb72ded20ece34a6207dc387b1d
willy836 <wilsonkimiri836@gmail.com>
2023-11-26 21:03:35 +0300

    Add RestaurantTable model

---
+++ models/RestaurantTable.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const RestaurantTableSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide table name"],
+      minlength: 3,
+      maxlength: 20,
+    },
+    image: {
+      type: String,
+      required: [true, "Please provide image url"],
+    },
+    desc: {
+      type: String,
+      required: [true, "Please provide description"],
+      minlength: 200,
+      maxlength: 500,
+    },
+    price: {
+      type: String,
+      required: [true, "Please provide price"],
+    },
+    tableSize: {
+      type: String,
+      required: [true, "Please provide table size"],
+    },
+    authorId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please provide author"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("RestaurantTable", RestaurantTableSchema);
